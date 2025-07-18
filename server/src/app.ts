a6254d2205d49278c179c14592545c97656243ea
PranavVadakkathiyil <vpranav8123@gmail.com>
2025-07-18 10:40:35 +0530

    Added models

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,12 +3,12 @@ import cors from 'cors'
 import cookiesParser from 'cookie-parser'
 const app = express()
 app.use(cors({
-    origin:["http://localhost:513"],
+    origin:["http://localhost:5173"],
     credentials:true
 }))
 app.use(express.urlencoded({limit:"16kb",extended:true}))
 app.use(express.static('public'))
 app.use(express.json({limit:"16kb"}))
-app.use(cookiesParser())
+app.use(cookiesParser()) 
 
 export default app
